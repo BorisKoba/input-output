0f87137c26865d331705e08b89399ad2a7626d02
BorisKoba <browi@mail.ru>
2023-11-28 18:30:14 +0300

    HW#48 executor

--- src/telran/net/ClientSessionHandler.java
+++ src/telran/net/ClientSessionHandler.java
@@ -2,32 +2,54 @@ package telran.net;
 import java.net.*;
 import java.io.*;
 public class ClientSessionHandler implements Runnable {
- Socket socket;
+ private static final long TOTAL_IDLE_TIMEOUT = 30000;
+Socket socket;
  ObjectInputStream reader;
  ObjectOutputStream writer;
  ApplProtocol protocol;
- public ClientSessionHandler(Socket socket, ApplProtocol protocol) throws Exception {
+ TcpServer tcpServer;
+ long totalIdleTime = 0;
+ public ClientSessionHandler(Socket socket, ApplProtocol protocol, TcpServer tcpServer) throws Exception {
 	 this.socket = socket;
 	 this.protocol = protocol;
+	 this.tcpServer = tcpServer;
 	 reader = new ObjectInputStream(socket.getInputStream());
 	 writer = new ObjectOutputStream(socket.getOutputStream());
  }
 	@Override
 	public void run() {
-		try {
-			while(true) {
-				Request request = (Request) reader.readObject();
-				Response response = protocol.getResponse(request);
-				writer.writeObject(response);
-				writer.reset();
+			while(!tcpServer.executor.isShutdown()) {
+				try {
+					Request request = (Request) reader.readObject();
+					Response response = protocol.getResponse(request);
+					writer.writeObject(response);
+					writer.reset();
+				} catch(SocketTimeoutException e) {
+					//for exit from readObject to another iteration of cycle
+					totalIdleTime += TcpServer.IDLE_TIMEOUT;
+					if(totalIdleTime > TOTAL_IDLE_TIMEOUT &&
+					tcpServer.clientsCounter.get() > tcpServer.nThreads) {
+						break;
+					}
+				}
+				catch (EOFException e) {
+					System.out.println("Client closed connection");
+					break;
+				} 
+				catch (Exception e) {
+					System.out.println("Abnormal closing connection");
+					break;
+				}
+			}
+			try {
+				socket.close();
+			} catch (IOException e) {
+				System.out.println(e);
+			} finally {
+				tcpServer.clientsCounter.decrementAndGet();
 			}
 			
-		} catch (EOFException e) {
-			System.out.println("Client closed connection");
-		} catch (Exception e) {
-			System.out.println("Abnormal closing connection");
-		}
+		} 
 
-	}
 
 }
